9d9a0a112963d95d91af8cd579abbc9d93126baf
psharma <psharma@solifi.com>
2026-09-15 17:33:11 +0530

    Adds a test case to verify if the key length and key bytes are encoded correctly.

--- src/test/java/tcpClient/EncoderTest.java
+++ src/test/java/tcpClient/EncoderTest.java
@@ -24,4 +24,13 @@ public class EncoderTest {
 
         assertArrayEquals(new byte[]{0, 0, 0, 0, 0, 0, 0, 1}, Arrays.copyOfRange(encodedMessage, 0, 8));
     }
+
+    @Test
+    void shouldEncodeKeyCorrectly() {
+        RawMessage rawMessage = new RawMessage(1L, "name".getBytes(), "Pranjal".getBytes());
+        byte[] encodedMessage = Encoder.encode(rawMessage);
+
+        assertArrayEquals(new byte[]{0, 0, 0, 4}, Arrays.copyOfRange(encodedMessage, 8, 12));
+        assertArrayEquals("name".getBytes(), Arrays.copyOfRange(encodedMessage, 12, 16));
+    }
 }
