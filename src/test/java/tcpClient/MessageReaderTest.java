b3ecf0208396286398548b0523c11ccbe018730e
pranjalS9 <p.arudiya9@gmail.com>
2026-09-16 13:28:09 +0530

    Verifies if MessageReader correctly separates two consecutive messages on stream

--- src/test/java/tcpClient/MessageReaderTest.java
+++ src/test/java/tcpClient/MessageReaderTest.java
@@ -24,4 +24,26 @@ public class MessageReaderTest {
 
         assertArrayEquals(encodedMessage, result);
     }
+
+    @Test
+    void shouldProperlyReadMultipleMessages() throws IOException {
+        RawMessage rawMessage1 = new RawMessage(1L, "name".getBytes(), "Pranjal".getBytes());
+        RawMessage rawMessage2 = new RawMessage(1L, "age".getBytes(), "23".getBytes());
+
+        byte[] encodedMessage1 = Encoder.encode(rawMessage1);
+        byte[] encodedMessage2 = Encoder.encode(rawMessage2);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        MessageSender.send(outputStream, encodedMessage1);
+        MessageSender.send(outputStream, encodedMessage2);
+
+        byte[] writtenMessages = outputStream.toByteArray();
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(writtenMessages);
+        byte[] result1 = MessageReader.read(inputStream);
+        byte[] result2 = MessageReader.read(inputStream);
+
+        assertArrayEquals(encodedMessage1, result1);
+        assertArrayEquals(encodedMessage2, result2);
+    }
 }
