684670b3f146d9cf202e563be26135e24a24a2bd
pranjalS9 <p.arudiya9@gmail.com>
2026-09-22 12:48:58 +0530

    Verifies if ConnectionReader completes multiple futures from consecutive responses

--- src/test/java/tcpClient/ConnectionReaderTest.java
+++ src/test/java/tcpClient/ConnectionReaderTest.java
@@ -30,4 +30,27 @@ public class ConnectionReaderTest {
 
         socket.close();
     }
+
+    @Test
+    void shouldReadMultipleResponsesAndCompleteTheFutureForAll() throws IOException, ExecutionException, InterruptedException {
+        MockAPIServer server = new MockAPIServer(0);
+        server.start();
+
+        Socket socket = new Socket("localhost", server.getPort());
+
+        RequestWaitlist waitList = new RequestWaitlist();
+        CompletableFuture<RawMessage> future1 = waitList.register(1L);
+        CompletableFuture<RawMessage> future2 = waitList.register(2L);
+
+        MessageSender.send(socket.getOutputStream(), Encoder.encode(new RawMessage(1L, "name".getBytes(), "Pranjal".getBytes())));
+        MessageSender.send(socket.getOutputStream(), Encoder.encode(new RawMessage(2L, "name".getBytes(), "Pranjal".getBytes())));
+
+        ConnectionReader reader = new ConnectionReader(socket.getInputStream(), waitList);
+        reader.start();
+
+        assertEquals(1L, future1.get().getReqId());
+        assertEquals(2L, future2.get().getReqId());
+
+        socket.close();
+    }
 }
